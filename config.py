ccd91aaeafc3ddb355345f04e418201bc206400c
farhan123853 <farhanmalikibrahim06@gmail.com>
2026-05-07 19:07:42 +0700

    fronend

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-key-mediqueue')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:@localhost/mediqueue_db')
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'database.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt-super-secret-mediqueue')
     JWT_ACCESS_TOKEN_EXPIRES = 86400 # 1 hari
